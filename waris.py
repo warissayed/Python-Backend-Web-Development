01e437ed5a09316bce86f122936e65a74c21b2fc
warissayed <warissayed72@gmail.com>
2024-07-21 20:51:34 +0530

    1st day of lerning python

---
+++ waris.py
@@ -0,0 +1,10 @@
+#Print function and escape sequences
+print("Day 1 - Python Print Function")
+print('waris sayed')
+print('welcome')
+name = "waris"
+print(name)
+print (name + "is learning python")
+age = 23 
+print(age ,  name) 
+print("hi.\nhow are you")
